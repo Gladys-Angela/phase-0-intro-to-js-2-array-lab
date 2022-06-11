8cf7b4a1ad78c220ce04328abb1adc2946c9675f
Gladys <gladysangelakamau@gmail.com>
2022-06-11 18:25:46 +0300

    commit tree 872f3f2c551f1ecd75b8ab31f8a6cacf8b587f7e

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph){
+    cats.push(Ralph)
+    return cats
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob)
+    return cats
+}
+function destructivelyRemoveLastCat(Bob, Garfield){
+    cats.pop(Bob, Garfield)
+    return cats
+}
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift(Milo)
+    return cats
+}
+function appendCat(Broom){
+    const Broom_1=[...cats,Broom]
+    return Broom_1
+}
+function prependCat(Arnold){
+    const Arnold_unshift=[Arnold,...cats]
+    return Arnold_unshift
+}
+function removeLastCat(){
+    const Broom = cats.slice(0,cats.length-1);
+    return Broom;
+}
+
+function removeFirstCat(){
+    const Arnold = cats.slice(1);
+    return Arnold;
+}
